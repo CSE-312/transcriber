e5b7c7f9b28a5c49e135e68a592d94f983d62fa6
zaida04 <nico.03727+github@gmail.com>
2025-03-01 17:22:01 -0500

    add out dir

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,15 +1,9 @@
 module.exports = {
     apps: [{
         name: 'transcriber',
-        script: 'npm',
-        args: 'start',
+        script: './dist/index.js',
         instances: 'max',
         exec_mode: 'cluster',
-        watch: false,
-        max_memory_restart: '1G',
-        merge_logs: true,
-        max_restarts: 10,
-        restart_delay: 4000,
         source_map_support: true,
         node_args: [
             '--enable-source-maps'
