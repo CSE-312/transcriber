6d5c0b38ec4fbbfe462dfddfc9bd172bcad13ce2
zaida04 <nico.03727+github@gmail.com>
2025-03-01 15:41:31 -0500

    update pm2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,15 +1,22 @@
 module.exports = {
-  apps: [
-    {
-      name: "trans",
-      script: "src/index.ts", // Entry point of your application
-      interpreter: "/home/nico/.bun/bin/bun",
-      instances: "max",
-      exec_mode: "cluster",
-      env: {
-        PORT: 5000,
-        NODE_ENV: "production",
-      },
-    },
-  ],
+    apps: [{
+        name: 'trans',
+        script: 'bun',
+        args: 'src/index.ts',
+        instances: 'max',
+        exec_mode: 'cluster',
+        watch: false,
+        max_memory_restart: '1G',
+        env: {
+            NODE_ENV: 'development'
+        },
+        env_production: {
+            NODE_ENV: 'production'
+        },
+        error_file: 'logs/err.log',
+        out_file: 'logs/out.log',
+        merge_logs: true,
+        max_restarts: 10,
+        restart_delay: 4000
+    }]
 };
--- src/middleware/rateLimiter.ts
+++ src/middleware/rateLimiter.ts
@@ -4,8 +4,8 @@ import logger from '../utils/logger';
 
 // Rate limiter for general API endpoints
 export const apiLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per windowMs
+    windowMs: 10 * 60 * 1000, // 15 minutes
+    max: 20, // Limit each IP to 100 requests per windowMs
     message: 'Too many requests from this IP, please try again later',
     standardHeaders: true,
     legacyHeaders: false,
